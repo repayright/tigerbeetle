0ff7acb1d2fc0d2be34709b7c273147880a0c4e7
sentientwaffle <sentientwaffle@gmail.com>
2023-08-17 14:13:08 -0700

    VSR: Extract Op math helpers

--- src/vsr/replica.zig
+++ src/vsr/replica.zig
@@ -4948,24 +4948,16 @@ pub fn ReplicaType(
         ///     %  op_checkpoint's trigger
         ///
         fn op_checkpoint_next(self: *const Self) u64 {
+            assert(vsr.Op.checkpoint_valid(self.op_checkpoint()));
             assert(self.op_checkpoint() <= self.commit_min);
             assert(self.op_checkpoint() <= self.op or
                 self.status == .recovering or self.status == .recovering_head);
-            assert(self.op_checkpoint() == 0 or
-                (self.op_checkpoint() + 1) % constants.lsm_batch_multiple == 0);
 
-            const op = if (self.op_checkpoint() == 0)
-                // First wrap: op_checkpoint_next = 8-2-1 = 5
-                constants.journal_slot_count - constants.lsm_batch_multiple - 1
-            else
-                // Second wrap: op_checkpoint_next = 5+8-2 = 11
-                // Third wrap: op_checkpoint_next = 11+8-2 = 17
-                self.op_checkpoint() + constants.journal_slot_count - constants.lsm_batch_multiple;
-            assert((op + 1) % constants.lsm_batch_multiple == 0);
-            // The checkpoint always advances.
-            assert(op > self.op_checkpoint());
+            const checkpoint_next = vsr.Op.checkpoint_after_checkpoint(self.op_checkpoint());
+            assert(vsr.Op.checkpoint_valid(checkpoint_next));
+            assert(checkpoint_next > self.op_checkpoint()); // The checkpoint always advances.
 
-            return op;
+            return checkpoint_next;
         }
 
         /// Returns the next op that will trigger a checkpoint.
@@ -4976,7 +4968,7 @@ pub fn ReplicaType(
         ///
         /// See `op_checkpoint_next` for more detail.
         fn op_checkpoint_next_trigger(self: *const Self) u64 {
-            return self.op_checkpoint_next() + constants.lsm_batch_multiple;
+            return vsr.Op.trigger_from_checkpoint(self.op_checkpoint_next());
         }
 
         fn checkpoint_id_for_op(self: *const Self, op: u64) ?u128 {
